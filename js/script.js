dd3297b5dd3b3c5f33a243d812ab31d7c202673b
Stelin <stelinduarte100@gmail.com>
2020-12-18 12:09:49 -0600

    Image bug fixed

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const lname = document.querySelector("#lname")
 const email = document.querySelector("#email")
 const password = document.querySelector("#password")
 const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
 button.addEventListener("click", comprobacion)
 
 function comprobacion(e){
@@ -38,7 +37,7 @@ function comprobacion(e){
 function vacio(parametro){
   const error = document.createElement("p")
   error.innerHTML = `
-  <img src="../images/icon-error.svg"></img> ${parametro.name} cannot be empty
+  <img src="images/icon-error.svg"></img> ${parametro.name} cannot be empty
   `
   form.insertBefore(error, form.children[15])
   setTimeout( ()=>{
@@ -49,7 +48,7 @@ function vacio(parametro){
 function emailError(){
   const error = document.createElement("p")
   error.innerHTML = `
-  <img src="../images/icon-error.svg"></img> Looks like this is not an email 
+  <img src="images/icon-error.svg"></img> Looks like this is not an email 
   `
   form.insertBefore(error, form.children[15])
   setTimeout( ()=>{
